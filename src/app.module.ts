56a60b5fe5cb03e1c663fb161989045c38b8ad75
Alja Čekada <alja.cekada@gmail.com>
2023-12-29 15:32:20 -0800

    set up custom logger middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LoggerMiddleware } from 'src/middleware/logger.middleware';
 
 @Module({
   imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  // logger middleware for console logging all requests to the server
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes({ path: '*', method: RequestMethod.ALL });
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import cookieParser = require('cookie-parser');
+import * as express from 'express';
+import Logging from './library/Logging';
+import { AllExceptionsFilter } from './helpers/GlobalExceptionFilter';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, {
+    bufferLogs: true,
+  });
+  app.useGlobalPipes(new ValidationPipe());
+  app.use(cookieParser());
+  // Setup to serve static files
+  app.use('/files', express.static('files'));
+
+  // Setup global filters
+  app.useGlobalFilters(new AllExceptionsFilter());
+
+  // Start the backend server
+  const PORT = process.env.PORT || 8080;
+  await app.listen(PORT);
+
+  Logging.info(`App is listening on: ${await app.getUrl()}`);
 }
 bootstrap();
